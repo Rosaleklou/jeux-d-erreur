64880406f03aa74473ec40e717802c9213cc2c83
Rosalelklou <rosaradiotv@gmail.com>
2021-10-18 16:16:09 +0200

    feat:fin du js premiere image d'erreur jeux

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const erreur = document.querySelector('#erreur');
+const modifié = document.querySelector('#modifié');
+// console.log(erreur);
+// console.log(modifié);
 
-var erreur ="";
-var origin="";
+erreur.addEventListener('click',function(){
+    erreur.style.opacity ="1";
+});
+// function rejouer(){
+//     if (erreur){
+//         style.opacity ="1"     
+//     }   
+// }  
+// rejouer();
+// console.log(rejouer);
+     
 
-function erreur(){
-    trouve=document.getElementById('erreur')
-
-}
